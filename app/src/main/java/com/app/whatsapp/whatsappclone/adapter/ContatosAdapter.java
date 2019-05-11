3c379cb29948373fd97f30156df39aa3853151f0
jvsena42 <jvsena16@gmail.com>
2019-05-11 14:40:00 -0500

    +Listando Contatos 1

---
+++ app/src/main/java/com/app/whatsapp/whatsappclone/adapter/ContatosAdapter.java
@@ -0,0 +1,49 @@
+package com.app.whatsapp.whatsappclone.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.whatsapp.whatsappclone.R;
+import com.app.whatsapp.whatsappclone.model.Usuario;
+
+import java.util.List;
+
+public class ContatosAdapter extends RecyclerView.Adapter<ContatosAdapter.MyViewHolder> {
+
+    private List<Usuario> contatos;
+    private Context context;
+
+    public ContatosAdapter(List<Usuario> listaContatos, Context c) {
+        this.contatos = listaContatos;
+        this.context = c;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemLista = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.adapter_contatos,viewGroup,false);
+        return new MyViewHolder(itemLista);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contatos.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
--- app/src/main/java/com/app/whatsapp/whatsappclone/fragment/ContatosFragment.java
+++ app/src/main/java/com/app/whatsapp/whatsappclone/fragment/ContatosFragment.java
@@ -3,16 +3,26 @@ package com.app.whatsapp.whatsappclone.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.app.whatsapp.whatsappclone.R;
+import com.app.whatsapp.whatsappclone.adapter.ContatosAdapter;
+import com.app.whatsapp.whatsappclone.model.Usuario;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContatosFragment extends Fragment {
+
+    private RecyclerView recyclerViewListaContatos;
+    private ContatosAdapter adapter;
+    private ArrayList<Usuario> listaContatos = new ArrayList<>();
 
 
     public ContatosFragment() {
@@ -24,7 +34,21 @@ public class ContatosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        //Configuracoes iniciais
+        recyclerViewListaContatos = view.findViewById(R.id.recyclerViewListaContatos);
+
+        //Configurar adapter
+        adapter = new ContatosAdapter(listaContatos, getActivity());
+
+        //Configurar recyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerViewListaContatos.setLayoutManager(layoutManager);
+        recyclerViewListaContatos.setHasFixedSize(true);
+        recyclerViewListaContatos.setAdapter(adapter);
+
+        return  view;
     }
 
 }
